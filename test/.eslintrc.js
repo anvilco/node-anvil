49d14b208e1b09deaeab57495be98655c333e2f3
newhouse <c.newhouse@gmail.com>
2020-09-08 12:13:10 -0400

    added bdd and other paterns we follow in web app

---
+++ test/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: '../.eslintrc.js',
+  env: {
+    mocha: true
+  },
+  globals: {
+    expect: 'readonly',
+    should: 'readonly',
+    sinon : 'readonly',
+    mount : 'readonly',
+    render : 'readonly',
+    shallow : 'readonly',
+    //*************************************************
+    // bdd-lazy-var
+    //
+    // In order to get around eslint complaining for now:
+    // https://github.com/stalniy/bdd-lazy-var/issues/56#issuecomment-639248242
+    $: 'readonly',
+    its: 'readonly',
+    def: 'readonly',
+    subject: 'readonly',
+    get: 'readonly',
+    sharedExamplesFor: 'readonly',
+    includeExamplesFor: 'readonly',
+    itBehavesLike: 'readonly',
+    is: 'readonly'
+    //
+    //*************************************************
+  }
+}

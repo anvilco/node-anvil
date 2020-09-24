e54784abb6b6ef05baae6b4978d17d30aacf10eb
newhouse <c.newhouse@gmail.com>
2020-09-24 15:39:38 -0400

    draft => isDraft

--- example/script/create-etch-packet.js
+++ example/script/create-etch-packet.js
@@ -19,7 +19,7 @@ async function main () {
   const streamFile = Anvil.prepareGraphQLFile(pathToFile)
 
   const variables = {
-    draft: false,
+    isDraft: false,
     isTest: true,
     signatureEmailSubject: 'Test Create Packet',
     signers: [

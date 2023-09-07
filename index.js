18498d67611774862fdf37fde80c35fc3b645f8f
newhouse <c.newhouse@gmail.com>
2023-09-07 16:26:56 -0400

    tidying up

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const Dist = require('./dist')
 
 // This file is mainly just for cleaning up the exports to be intuitive/commonjs
 module.exports = Dist.default
+// This is here just for backwards compatibilty. Should be removed at next
+// major verison to avoid a breaking change
 module.exports.default = Dist.default

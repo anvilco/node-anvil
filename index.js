c9cb9c49b00a66e1f640a94d4b400ecd19578395
newhouse <c.newhouse@gmail.com>
2023-09-07 11:31:30 -0400

    fix exports

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+const Dist = require('./dist')
+
 // This file is mainly just for cleaning up the exports to be intuitive/commonjs
-module.exports = require('./dist')
+module.exports = Dist.default
+module.exports.default = Dist.default
